53199a09f22bb9e77a414392d048ab29d7b526e1
Mzg. <jchallaksantos@gmail.com>
2024-06-05 08:31:06 -0300

    DeForm

--- src/main/java/app/javafx/project/DepartmentController.java
+++ src/main/java/app/javafx/project/DepartmentController.java
@@ -1,23 +1,31 @@
 package app.javafx.project;
 
+import gui_utils.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import model_entities.Department;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import model.entities.Department;
 import model_services.DepartmentService;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class DepartmentController implements Initializable {
 
-    private DepartmentService service;
+    private DepartmentService departmentService;
 
     @FXML
     private TableView<Department> departmentTableView;
@@ -33,38 +41,49 @@ public class DepartmentController implements Initializable {
 
     private ObservableList<Department> obsList;
 
-    @FXML
-    public void OnBtNewAction(){
-        System.out.println("oi");
-
-    }
-
-    public void setDepartmentService(DepartmentService service){
-        this.service = service;
-    }
-
-
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        initializeNodes();
-
+        initializeTableView();
     }
 
-    private void initializeNodes() {
+    private void initializeTableView() {
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
     }
 
-    public void updateTableView(){
-       if (service == null) {
-           throw new IllegalStateException("Service = Null");
-       }
-        List list = service.findAll();
-        obsList = FXCollections.observableArrayList(list);
-        departmentTableView.setItems(obsList);
-
-
-       }
+    public void setDepartmentService(DepartmentService departmentService) {
+        this.departmentService = departmentService;
     }
 
+    public void updateTableView() {
+        if (departmentService == null) {
+            throw new IllegalStateException("Department Service is null");
+        }
+        List<Department> departmentList = departmentService.findAll();
+        obsList = FXCollections.observableArrayList(departmentList);
+        departmentTableView.setItems(obsList);
+    }
+
+    @FXML
+    public void onBtNewAction(ActionEvent event) {
+        Stage parentStage = Utils.currentStage(event);
+        openDialogForm("DepartmentForm.fxml", parentStage);
+    }
+
+    private void openDialogForm(String fxmlPath, Stage parentStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Pane pane = loader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Enter Department data");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
